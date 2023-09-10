831347734f1ae18a402966eb0a5bc1b3aa1c7e14
renjith.pk <renjith83@gmail.com>
2023-09-10 19:46:05 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="kcontext",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        # List your project's dependencies here
-    ],
+    name='kcontext',
+    version='0.1.0',
+    py_modules=['kcontext'],
+    entry_points={
+        'console_scripts': [
+            'kcontext=kcontext:main',
+        ],
+    },
+    author='renjith.pk',
+    description='A Kubernetes context manager tool',
+    url='https://github.com/renjithpk/kcontext',
+    license='MIT',
 )
